7029de6615bb00a1421864b71764eeed7214bd05
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2015-06-30 11:01:53 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -33,15 +33,16 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;	//Exporta la definición de la tabla Quiz
 
 //sequelize.sync() cre e inicializa tablas de preguntas en las Bases de Datos
-sequelize.sync().success(function(){
-	//Success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function (count){
+sequelize.sync().then(function(){
+	//Then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function (count){
 		if (count === 0) {	///la tabla se inicializa solo si está vacia
-			Quiz.create({
-				pregunta:   'Capital de Italia',
-				respuesta:   'Roma' 
-			})
-			.success(function(){console.log('Base de Datos inicializa')});
+			 Quiz.bulkCreate(  
+		        [	{pregunta: 'Capital de Italia',   respuesta: 'Roma'}, 
+        		   	{pregunta: 'Capital de Portugal', respuesta: 'Lisboa'} 
+        		]
+		
+			).then(function(){console.log('Base de Datos inicializa')});
 		};
 	});
 });
