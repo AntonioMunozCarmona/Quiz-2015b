acd9787df89cebfc856eb35c8536c33ed7f7ecbe
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2015-07-07 20:41:10 +0200

    Validacion

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,22 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId',quizController.load); //Se carga siempre que venga un :quizID en un GET
+router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
-router.get('/quizes',                    	quizController.index);
-router.get('/quizes/:quizId(\\d+)',      	quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
-router.get('/quizes/new',					quizController.new);
-router.post('/quizes/create',				quizController.create);
-
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 /* GET página del autor. */
 router.get('/author', function(req, res) {
-  res.render('author', { title: 'Quiz' });
+  res.render('author', { title: 'Quiz', errors: []}); 
 });
 
 module.exports = router;
