027da5c9a2edbe84d2021be2ee84b6625fa394bf
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2015-07-07 20:41:10 +0200

    Validacion

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,60 +1,71 @@
 var models = require('../models/models.js');
 
 //Autoload =>Factoriza el código si la ruta incluye :quizId
-exports.load = function (req, res, next, quizId) {
+exports.load = function(req, res, next, quizId) {
   models.Quiz.find(quizId).then(
-    function(quiz){
+    function(quiz) {
       if (quiz) {
         req.quiz = quiz;
         next();
-      } else {
-        next(new Error('No existe :quizId= '+quizId));
-        }
+      } else{next(new Error('No existe quizId=' + quizId));}
     }
-    ).catch(function(error){
-      next(error); });
+  ).catch(function(error){next(error);});
 };
-
 
 // GET /quizes
 exports.index = function(req, res) {
-
   var search = req.query.search == undefined?"":req.query.search.replace(" ", "%");
-  models.Quiz.findAll({where: ["pregunta like ?", '%'+search+'%'], order: 'pregunta ASC'}).then(function(quizes){ 
-      res.render('quizes/index.ejs', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+  models.Quiz.findAll({where: ["pregunta like ?", '%'+search+'%'], order: 'pregunta ASC'})
+  .then(function(quizes){ 
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+  }
+  ).catch(function(error){next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-    res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
   var resultado = 'Incorrecto';
   if (req.query.respuesta === req.quiz.respuesta) {
-      resultado = 'Correcto';
-    } 
-  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+    resultado = 'Correcto';
+  }
+  res.render(
+    'quizes/answer', 
+    { quiz: req.quiz, 
+      respuesta: resultado, 
+      errors: []
+    }
+  );
 };
 
 //GET /quizes/new
 exports.new = function(req, res) {
-  var quiz = models.Quiz.build(         // Crea objeto quiz
+  var quiz = models.Quiz.build( // crea objeto quiz 
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 //POST /quizes/create
 exports.create = function(req, res) {
-  var quiz = models.Quiz.build(req.body.quiz);
+  var quiz = models.Quiz.build( req.body.quiz );
 
-  // Guarda en la Base de Datos los campor pregunta y respuesta de quiz
-  quiz.save({fields:["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })    // Redireccion HTTP al URL relativo de la lista de preguntas (/quizes)
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
 };
