27eca65ceb5e5c58e827c8a07c245f41ba8cc253
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2015-07-09 19:45:20 +0200

    Indice Temático-P2P Mod.8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,7 +45,7 @@ exports.answer = function(req, res) {
 //GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( // crea objeto quiz 
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
   );
 
   res.render('quizes/new', {quiz: quiz, errors: []});
@@ -63,7 +63,7 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
@@ -80,6 +80,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
@@ -88,8 +89,8 @@ exports.update = function(req, res) {
       if (err){
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors });
       }else {
-        req.quiz      //save guarda campos pregunta y respuesta en la BD
-        .save({fields: ["pregunta", "respuesta"]})
+        req.quiz      //save guarda campos pregunta, respuesta y tema en la BD
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){res.redirect('/quizes')})   //Redirecciona a lista de preguntas
       }
     }
